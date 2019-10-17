ee6531a543d8c32c1b3087b1d3ce3c8af995f0b2
Matthew Cheng <matthewkit@gmail.com>
2019-10-17 11:50:36 +0100

    added names and connections to my_group

--- group.py
+++ group.py
@@ -2,4 +2,33 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {'Jill':{},'Zalika':{},'John':{},'Nash':{}}
+
+
+Jill = {
+	'name':'Jill',
+	'age':26, 
+	'job':'Biologist', 
+	'connections':['me','friend','partner','NA'], 
+	'Map':list(my_group.keys())
+	}
+Zalika = {
+	'name':'Zalika',
+	'age':28, 'job':'Artist',
+	'connection':['friend','me','NA','NA'],
+	'Map':list(my_group.keys())
+	}
+John = {
+	'name':'John',
+	'age':27, 'job':'Writer',
+	'connection':['partner','NA','me','NA'],
+	'Map':list(my_group.keys())
+	}
+Nash = {
+	'name':'Nash',
+	'age':34, 'job':'Chef',
+	'connection':['NA','landlord','cousin','me'],
+	'Map':list(my_group.keys())
+	}
+
+my_group = {'Jill':Jill,'Zalika':Zalika,'John':John,'Nash':Nash}
